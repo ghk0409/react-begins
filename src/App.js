a6ecb22c26f1229655cbddde4227c3861fcf01bb
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-12-08 21:16:28 +0900

    useEffect 기본
    첫 렌더링 시에만 실행되게끔 만들어주는 useEffect() 사용 테스트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const onClick = () => setCounter((prev) => prev + 1);
+    console.log("rerendering!! i run all the time");
+
+    useEffect(() => {
+        console.log("Call the API");
+    }, []);
     return (
         <div>
-            <h1 className={styles.title}>Welcome ReactJS!!</h1>
-            <Button text={"WOW!!"} />
+            <h1 className={styles.title}>Welcome ReactJS!! {counter}</h1>
+            {/* <Button text={"WOW!!"} /> */}
+            <button onClick={onClick}></button>
         </div>
     );
 }
