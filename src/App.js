c061e8d8e59f91ed5d3f6a74e02f094f334f8ee2
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-12-11 17:37:16 +0900

    useEffect의 Cleanup function 테스트
    메모리 누수 방지를 위한 cleanup function 구조 및 테스트

--- src/App.js
+++ src/App.js
@@ -2,36 +2,55 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState("");
-
-    const onClick = () => setCounter((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log("rerendering!! i run all the time");
-
-    // 처음 한 번만 실행
+function Hello() {
     useEffect(() => {
-        console.log("Call the API");
+        console.log("created :)");
+        return () => console.log("destroyed :(");
     }, []);
-    // keyword가 5글자 이상일 경우에만 실행 (deps -> keyword 설정)
-    useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log("Search for", keyword);
-        }
-    }, [keyword]);
+    return <h1>Hellooo~</h1>;
+}
+
+function App() {
+    // const [counter, setCounter] = useState(0);
+    // const [keyword, setKeyword] = useState("");
+
+    // const onClick = () => setCounter((prev) => prev + 1);
+    // const onChange = (event) => setKeyword(event.target.value);
+    // console.log("rerendering!! i run all the time");
+
+    // // 처음 한 번만 실행
+    // useEffect(() => {
+    //     console.log("Call the API");
+    // }, []);
+    // // keyword가 5글자 이상일 경우에만 실행 (deps -> keyword 설정)
+    // useEffect(() => {
+    //     if (keyword !== "" && keyword.length > 5) {
+    //         console.log("Search for", keyword);
+    //     }
+    // }, [keyword]);
+
+    // return (
+    //     <div>
+    //         <input
+    //             placeholder="Search for Movie!!"
+    //             type="text"
+    //             value={keyword}
+    //             onChange={onChange}
+    //         />
+    //         <h1 className={styles.title}>{counter}</h1>
+    //         {/* <Button text={"WOW!!"} /> */}
+    //         <button onClick={onClick}>BUTTON</button>
+    //     </div>
+    // );
+
+    // cleanUp 예시
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
 
     return (
         <div>
-            <input
-                placeholder="Search for Movie!!"
-                type="text"
-                value={keyword}
-                onChange={onChange}
-            />
-            <h1 className={styles.title}>{counter}</h1>
-            {/* <Button text={"WOW!!"} /> */}
-            <button onClick={onClick}>BUTTON</button>
+            <button onClick={onClick}>{showing ? "Show" : "Hide"}</button>
+            {showing ? <Hello /> : null}
         </div>
     );
 }
