a60c7106c9d7b361fb9b96d1836e31d09148ef89
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-12-11 16:35:41 +0900

    useEffect의 deps 설정
    deps 설정을 통한 특정 기능 실행 제어

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("");
+
     const onClick = () => setCounter((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
     console.log("rerendering!! i run all the time");
 
+    // 처음 한 번만 실행
     useEffect(() => {
         console.log("Call the API");
     }, []);
+    // keyword가 5글자 이상일 경우에만 실행 (deps -> keyword 설정)
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("Search for", keyword);
+        }
+    }, [keyword]);
+
     return (
         <div>
-            <h1 className={styles.title}>Welcome ReactJS!! {counter}</h1>
+            <input
+                placeholder="Search for Movie!!"
+                type="text"
+                value={keyword}
+                onChange={onChange}
+            />
+            <h1 className={styles.title}>{counter}</h1>
             {/* <Button text={"WOW!!"} /> */}
-            <button onClick={onClick}></button>
+            <button onClick={onClick}>BUTTON</button>
         </div>
     );
 }
