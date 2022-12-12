aa02e6a61fdb678c4a0217938285e0cc4312d879
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-12-12 11:40:37 +0900

    ToDo 리스트 생성
    - 이전 state 보관하는 list state

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-function Hello() {
-    useEffect(() => {
-        console.log("created :)");
-        return () => console.log("destroyed :(");
-    }, []);
-    return <h1>Hellooo~</h1>;
-}
+import { useState } from "react";
 
 function App() {
-    // const [counter, setCounter] = useState(0);
-    // const [keyword, setKeyword] = useState("");
-
-    // const onClick = () => setCounter((prev) => prev + 1);
-    // const onChange = (event) => setKeyword(event.target.value);
-    // console.log("rerendering!! i run all the time");
-
-    // // 처음 한 번만 실행
-    // useEffect(() => {
-    //     console.log("Call the API");
-    // }, []);
-    // // keyword가 5글자 이상일 경우에만 실행 (deps -> keyword 설정)
-    // useEffect(() => {
-    //     if (keyword !== "" && keyword.length > 5) {
-    //         console.log("Search for", keyword);
-    //     }
-    // }, [keyword]);
-
-    // return (
-    //     <div>
-    //         <input
-    //             placeholder="Search for Movie!!"
-    //             type="text"
-    //             value={keyword}
-    //             onChange={onChange}
-    //         />
-    //         <h1 className={styles.title}>{counter}</h1>
-    //         {/* <Button text={"WOW!!"} /> */}
-    //         <button onClick={onClick}>BUTTON</button>
-    //     </div>
-    // );
-
-    // cleanUp 예시
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
+    // 투두 state와 투두리스트 state 생성
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    // input value => toDo
+    const onChange = (event) => setToDo(event.target.value);
+    // :toDo값을 toDos리스트에 저장
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        // :toDos에 toDo를 넣은 후 빈 값으로 초기화
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    };
+    console.log(toDos);
 
     return (
         <div>
-            <button onClick={onClick}>{showing ? "Show" : "Hide"}</button>
-            {showing ? <Hello /> : null}
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={toDo}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Write your to do!"
+                />
+                <button>Add To Do</button>
+            </form>
         </div>
     );
 }
